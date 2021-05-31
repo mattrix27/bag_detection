99276f0dcd9c0729de67f5f73c8b1e99b262989c
Matthew Tung <mtung1998@gmail.com>
2021-05-31 08:19:15 -0400

    modify way to get ZED data from cam

--- scripts/bag_path_detection.py
+++ scripts/bag_path_detection.py
@@ -7,7 +7,8 @@ import cv2
 
 import message_filters
 from std_msgs.msg import UInt16
-from sensor_msgs.msg import Image, CompressedImage
+from sensor_msgs.msg import Image, CompressedImage, PointCloud2
+import sensor_msgs.point_cloud2 as pc2
 from geometry_msgs.msg import Point
 from visualization_msgs.msg import Marker
 from bag_detection.msg import PathPos
@@ -23,10 +24,6 @@ class bagPathModule:
     BAG_POS_TOPIC          = rospy.get_param("bag_path_detection/path_pos_topic")
     MODE_TOPIC             = rospy.get_param("bag_path_detection/mode_topic")
 
-    LOWER_COLOR            = rospy.get_param("bag_path_detection/lower_color_range")
-    UPPER_COLOR            = rospy.get_param("bag_path_detection/upper_color_range")
-    AREA                   = rospy.get_param("bag_path_detection/path_area")
-
     SIDE                   = rospy.get_param("bag_path_detection/side")
     MODE                   = 0
 
@@ -34,9 +31,9 @@ class bagPathModule:
         # Initialize your publishers and
         # subscribers here
         self.bag_rgb_camera_sub = message_filters.Subscriber(self.BAG_RGB_CAMERA_TOPIC, Image)
-        self.bag_depth_camera_sub = message_filters.Subscriber(self.BAG_DEPTH_CAMERA_TOPIC, Image)
+        self.bag_depth_camera_sub = message_filters.Subscriber(self.BAG_DEPTH_CAMERA_TOPIC, PointCloud2)
         self.ts = message_filters.TimeSynchronizer([self.bag_rgb_camera_sub, self.bag_depth_camera_sub], 10)
-        self.ts.registerCallback(react_to_camera)
+        self.ts.registerCallback(self.react_to_camera)
 
         self.mode_sub = rospy.Subscriber(self.MODE_TOPIC, UInt16, callback=self.update_mode)
         self.image_pub = rospy.Publisher(self.TEST_CAMERA_TOPIC, Image, queue_size=10)
@@ -45,9 +42,10 @@ class bagPathModule:
         self.last_row = (0,0)
 
         self.bridge = CvBridge()
-        self.h_matrix, self.status = self.makeHomography()
-
+	
+        self.gtf = None
         self.gtf = util.get_gtf()
+
         if (self.SIDE == 1):
            self.corner = (0,720)
         else:
@@ -56,13 +54,16 @@ class bagPathModule:
 
     def react_to_camera(self, rgb_data, depth_data):
         try:
-            rgb_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-            depth_image = self.bridge.imgmsg_to_cv2(data)
+            rgb_image = self.bridge.imgmsg_to_cv2(rgb_data, "bgr8")
+            point_cloud = pc2.read_points(depth_data.data, skip_nans=True, field_names=('x', 'y', 'z'))
         except CvBridgeError as e:
             print(e)
 	
-        bboxes = util.gtf_detect_objects(np.array(rgb_image))
-
+        try:
+            bboxes = util.gtf_detect_objects(self.gtf, np.array(rgb_image))
+        except:
+            print("MODEL NOT LOADED")
+        #print(point_cloud[0][1])
         
         #self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, encoding="passthrough"))
 
